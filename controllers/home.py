b7c700646d57e0d58020f3fb0d33429ea7aa8d4e
José Hernández <jhbez@outlook.com>
2019-10-16 16:27:10 -0500

    Remove index cover and caption for drawing and photography

--- controllers/home.py
+++ controllers/home.py
@@ -19,10 +19,10 @@ from db import drawing, photography
 
 def home_page():
     aw = [
-        {
-            'imgSrc': '/static/images/cover.jpg',
-            'captionText': "I'm artist, showing in art those that many hide, those who know and do not speak, see it and do not show",
-        }
+        #{
+        #    'imgSrc': '/static/images/cover.jpg',
+        #    'captionText': "I'm artist, showing in art those that many hide, those who know and do not speak, see it and do not show",
+        #}
     ]
     
     a = drawing.Drawing().get_all(limit=1)
@@ -30,7 +30,7 @@ def home_page():
         a = a[0]
         aw.append({
             'imgSrc': "/drawing/image/{}/{}".format(a.id, a.artworks[0].name),
-            'captionText': "Artwork is the concep"
+            #'captionText': "Artwork is the concep"
         })
 
     p = photography.Photography.get_all(limit=1)
@@ -38,6 +38,6 @@ def home_page():
         p = p[0]
         aw.append({
             'imgSrc': "/photography/image/{}/{}".format(p.id, p.artworks[0].name),
-            'captionText': "It is not the camera or the lens, it is the right time to take a picture"
+            #'captionText': "It is not the camera or the lens, it is the right time to take a picture"
         })
     return render_template("index.html", artworks = aw);
